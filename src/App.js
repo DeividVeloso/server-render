53e4fbb5768a8c085afaf28759cdf2f2b14661d0
Deivid Veloso <veloso.deivid@gmail.com>
2018-05-03 14:48:47 -0300

    add plugins to bootstrap.js to run react-loadable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
+import Loadable from 'react-loadable';
+
 import logo from './logo.svg';
 import './App.css';
+
+const AsyncComponent = Loadable({
+  loader: () => import("./SomeComponent"),
+  loading: () => <div>loading...</div>,
+});
 
 class App extends Component {
   render() {
@@ -13,6 +20,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <AsyncComponent/>
       </div>
     );
   }
